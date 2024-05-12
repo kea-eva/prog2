cfeefd6a268580e9263ee3cf25211f12c1e0d295
kea-eva <167180740+kea-eva@users.noreply.github.com>
2024-05-12 23:33:16 +0200

    Create protected__password.py
    Protected password: __password

---
+++ protected__password.py
@@ -0,0 +1,24 @@
+class UserInfo:
+    def __init__(self, username, password):
+        self.__username = username  # protected has two underscores
+        self.__password = password
+
+    def validate_password(self, password):
+        return password == self.__password
+
+    def change_password(self, current_password, new_password):
+        if self.validate_password(current_password):
+            self.__password = new_password
+            print("Password changed successfully.")
+        else:
+            print("Incorrect current password.")
+
+# Creating a user
+user = UserInfo("alice", "securepassword")
+
+# Accessing protected variable (do NOT do like this!!)
+print(user._UserInfo__password)
+
+# Using methods to interact with private variables
+user.change_password("incorrect", "newpassword")
+user.change_password("securepassword", "newpassword")
